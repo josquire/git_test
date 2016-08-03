617f65269e57bf078e026c1bb03bec31c66f11d9
Jo Squire <mjsquire@digis.net>
2016-08-03 12:18:20 -0600

    more test code

--- examples.rb
+++ examples.rb
@@ -1,3 +1,7 @@
 puts "Whats your name?"
 name = gets.strip
 puts "Hello #{name}"
+
+puts "Whats your age?"
+age = gets.to_i
+puts "#{age}? you are old!"
--- test.rb
+++ test.rb
@@ -1,1 +1,2 @@
 puts "testing"
+puts 'this is github testing'
